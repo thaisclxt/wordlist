d7b652ef137404f19ae94fc742a6effb54d64caa
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-06-13 14:59:26 -0300

    Corrigir méteodos

--- main.py
+++ main.py
@@ -1,5 +1,64 @@
 from random import randrange
-import string
+
+
+# thais
+def getAlpha(keyWord):
+    value = randrange(2)
+
+    if keyWord.isalpha() and not keyWord.isupper():
+        if value == 0:
+            return keyWord
+
+        elif value == 1:
+            return keyWord[:len(keyWord) // 2]
+
+        elif value == 2:
+            return keyWord[len(keyWord) // 2:]
+
+    return ''
+
+
+# UFT
+def getUpper(keyWord):
+    if keyWord.isupper():
+        return keyWord
+    return ''
+
+
+# 2022
+def getNumber(keyWord):
+    value = randrange(1)
+
+    if keyWord.isdigit():
+        if value == 0:
+            return keyWord
+
+        elif value == 2:
+            return keyWord[len(keyWord) // 2:]
+
+    return ''
+
+
+# Segurança e Auditoria de Sistemas / Ciência da Computação
+def getTitle(keyWord):
+    newWord = []
+    value = randrange(2)
+
+    if " " in keyWord:
+        for word in keyWord.split(" "):
+            if word.istitle():
+                newWord.append(word[0])
+
+        if value == 0:
+            return ''.join(newWord)
+
+        elif value == 1:
+            return '.'.join(newWord)
+
+        elif value == 2:
+            return '-'.join(newWord)
+
+    return ''
 
 
 # Lê cada linha do arquivo `wordList.txt` e as retorna
@@ -32,24 +91,10 @@ def main():
 
         # Para cada palavra em `keyWord` será gerada uma correspondente
         for keyword in keyWords:
-
-            # UFT
-            if keyword.isupper():
-                newWord.append(keyword)
-
-            # thais
-            elif keyword.isalpha():
-                newWord.append(keyword)
-
-            # 2022
-            if keyword.isdigit():
-                newWord.append(keyword)
-
-            # Segurança e Auditoria de Sistemas / Ciência da Computação
-            if " " in keyword:
-                for word in keyword.split(" "):
-                    if word.istitle():
-                        newWord.append(word[0])
+            newWord.append(getAlpha(keyword))
+            newWord.append(getUpper(keyword))
+            newWord.append(getNumber(keyword))
+            newWord.append(getTitle(keyword))
 
         wordList.append("".join(newWord))
 
