5b1624a452e9758d59d6cf8b4aee9aedac4a90c9
thais.calixto <thais.calixto@mail.uft.edu.br>
2022-06-13 10:42:21 -0300

    Refatorar código

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from random import randrange
+import string
 
 
-# Lê as palavras do arquivo `wordList.txt` e as retorna
+# Lê cada linha do arquivo `wordList.txt` e as retorna
 def getKeyWords():
     file = open("keyWords.txt", "r")
     keyWords = file.read().split("\n")
@@ -11,34 +11,24 @@ def getKeyWords():
     return keyWords
 
 
-# Gera novas palavras a partir do arquivo `wordList.txt` e as retorna
-def generateWords():
-    wordList = []
-    keyWords = getKeyWords()
+# Gera uma nova palavra a partir de `word`
+def getNewWord(keyWord):
+    newWord = []
 
-    for _ in range(int(input("Quantas palavras você deseja gerar? "))):
-        newWord = []
-        for word in keyWords:
+    # Verifica se `keyWord` é uma frase
+    if " " in keyWord:
+        for word in keyWord.split(" "):
+            pass
+    else:
+        for character in keyWord:
+            pass
 
-            # Adiciona duas letras randomicamente de cada palavra
-            newWord.append(word.upper()[randrange(len(word))])
-            newWord.append(word.upper()[randrange(len(word))])
-
-        # Adiciona dois números randomicamente ao final da palavra
-        newWord.append(str(randrange(10)))
-        newWord.append(str(randrange(10)))
-
-        # Adiciona a nova palavra em `wordList`
-        wordList.append("".join(newWord))
-
-    return wordList
+    return "".join(newWord)
 
 
 # Acessa ou cria um arquivo chamado `wordList.txt`, se já não existir, e escreve as palavras geradas
-def exportWordList():
+def exportWordList(wordList):
     file = open("wordList.txt", "w+")
-
-    wordList = generateWords()
 
     for word in wordList:
         file.write(word)
@@ -48,7 +38,16 @@ def exportWordList():
 
 
 def main():
-    exportWordList()
+    wordList = []
+    keyWords = getKeyWords()
+
+    for _ in range(int(input("Quantas palavras você deseja gerar? "))):
+
+        # Para cada palavra em `keyWord` será gerada uma correspondente
+        for word in keyWords:
+            wordList.append(getNewWord(word))
+
+    exportWordList(wordList)
 
 
 if __name__ == '__main__':
